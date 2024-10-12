01d69271124e12d8c9e96c30417fccc7b06fc4f1
Sabrabeta <150891203+Sabrabeta@users.noreply.github.com>
2024-10-12 13:57:13 -0700

    new_file

--- set_dictionary/sqrt.py
+++ set_dictionary/sqrt.py
@@ -9,3 +9,10 @@ def square_matrix_simple(matrix):
             result[i][j] = matrix[i][j] ** 2
 
     return result
+
+# Example usage:
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+new_matrix = square_matrix_simple(matrix)
+
+print(new_matrix)
+print(matrix)
