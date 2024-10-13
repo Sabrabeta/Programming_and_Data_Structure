74f72ce3e9372ea81ba329b1cafdd060b18e82c5
Sabrabeta <150891203+Sabrabeta@users.noreply.github.com>
2024-10-12 23:19:19 -0700

    replace

---
+++ set_dictionary/replace.py
@@ -0,0 +1,17 @@
+def search_replace(my_list , search, replace):
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+    return new_list
+
+#example usage
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list) #output: [1, 89, 3, 4, 5, 4, 89, 1, 1, 4, 89]
+print(my_list)  #output: [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
